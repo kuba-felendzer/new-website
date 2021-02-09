c7a329962a5d9c3658577092ac1b6c0f6f209ace
Zachary Pike <zachturnpike@gmail.com>
2021-02-08 22:18:21 -0500

    Am very dumb, made file interperter for nav no longer needed by being smart for once

--- getnav.js
+++ getnav.js
@@ -4,13 +4,8 @@ module.exports = function (socket, msg, con) {
     //sends nav
     if (msg.loadnav == true) {
         //adapt, improvise, overcome
-        var data = []
         var pages = JSON.parse(fs.readFileSync("storage/pages.json"))
 
-        pages.forEach(element => {
-            data.push({"pagename": element[0], "pagepath": element[1] });
-        });
-
-        socket.emit('loadnav', data)
+        socket.emit('loadnav', pages)
     }
 }
