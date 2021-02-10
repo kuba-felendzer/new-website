bc9516e6853b94effcf3d982b06a467c44a65732
LividPhysics <66078440+kuba-felendzer@users.noreply.github.com>
2021-02-10 13:12:24 -0600

    Potentially add stuff
    May or may not work

--- chatroom.js
+++ chatroom.js
@@ -15,6 +15,9 @@ module.exports = function (io, msg, tokens, con) {
                     let data = getUserInfo(token, tokens)
 
                     //broadcast the message to everyone
+                    if(data.rname === data.uname) {
+                        io.emit("chatroom", {"type": "newmsg", "sendername": data.rname, "text": text, "sendericon": data.icon})
+                    }
                     io.emit("chatroom", {"type": "newmsg", "sendername": data.rname, "senderusername": data.uname, "text": text, "sendericon": data.icon})
                 }
                 break;
